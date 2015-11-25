8ea97c0c88de8dbe03e8ce014646ac8bdc1c3187
fumingivy <mm.fu@mail.utoronto.ca>
2015-11-25 17:21:24 -0500

    updated author and names in exercise 2

--- exercise2.py
+++ exercise2.py
@@ -6,10 +6,9 @@ Computer-based immigration office for Kanadia
 
 """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
-__copyright__ = "2015 Susan Sim"
-__license__ = "MIT License"
+__author__ = 'Piaoyao Shi & Zixiao Yang & Ming Fu '
+__email__ = "piaoyao.shi@mail.utoronto.ca & zeke.yang@mail.utoronto.ca  & mm.fu@mail.utoronto.ca"
+__copyright__ = "2015 Piaoyao Shi & Zixiao Yang & Ming Fu"
 
 import re
 import datetime
