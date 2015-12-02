5a03cb109216404cc29528257c78d59635017c30
fumingivy <mm.fu@mail.utoronto.ca>
2015-12-01 21:42:55 -0500

    scenario in  test exercise 2

--- test_exercise2.py
+++ test_exercise2.py
@@ -9,9 +9,7 @@ __copyright__ = "2015 Piaoyao Shi & Zixiao Yang & Ming Fu"
 __status__ = "Prototype"
 
 # imports one per line
-import pytest
-import os
-from exercise2 import decide
+import
 
 DIR = "test_jsons/"
 os.chdir(DIR)
@@ -23,5 +21,23 @@ def test_returning():
     """
     assert decide("test_returning_citizen.json", "countries.json") ==\
         ["Accept", "Accept", "Quarantine"]
+
+
+def test_accept():
+ """test accept:
+ 1. citizen
+    a. A valid passport
+    b. home country is Kanadia  (KAN)
+    c. accept mismatch between uppercase and lowercase
+
+ 2. visitor
+     a. complete entry record
+     b. location mentioned in the entry record is known
+     c. home country is not Kanadia  (KAN)
+     d. has a passport from a country from which a visitor visa is required
+     e. has a valid visa
+     f. accept mismatch between uppercase and lowercase
+
+ """
 
 
